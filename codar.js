293c777c32d3e1e4cfd913228a3c24962c08eb5f
Luana Karoline <luanakarolineliramateus2021@gmail.com>
2023-10-31 20:45:11 -0300

    inicio

---
+++ codar.js
@@ -0,0 +1,13 @@
+/*
+function clicou(){
+  window.alert('você clicou')
+}
+
+function nome(){
+    prompt('Digite seu nome')
+}
+*/
+function iniciou(){
+    let nome= window.prompt('Digite seu nome')
+    alert(`ola!${nome} ! é um prazer te conhecer`);
+}
